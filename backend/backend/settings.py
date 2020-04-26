c8aa3702cf0dc8faf3fd6ad112274824172b8bf2
PrzemcioTbg <przemek123454@gmail.com>
2020-04-26 17:29:38 +0200

    initial commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w)h=zy!x7ou)hie%=3%n029)t0=sn^-b%p)%w70wzp_j2e=_1^'
+SECRET_KEY = '4tdo$s*hd=-2k+)tbc9yh@%q(p#i(%-443u4(sxzmpndev&^$$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
