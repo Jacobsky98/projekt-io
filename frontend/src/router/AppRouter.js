06a22244d03c30812be6b1e81d3a25642533e33b
MWojtasik <misiekwojtasik@gmail.com>
2020-05-10 14:20:15 +0200

    mwojtasik changes (#7)

--- frontend/src/router/AppRouter.js
+++ frontend/src/router/AppRouter.js
@@ -1,41 +1,61 @@
 import React, {useState, useEffect} from 'react';
-import {Switch, Route } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import {Switch, Route, Redirect} from 'react-router-dom';
+import {useSelector} from 'react-redux';
 import LoginPage from '../pages/commons/LoginPage';
 import RegisterPage from '../pages/commons/RegisterPage';
 import StudentCourses from '../pages/student/StudentCourses';
+import {ROLES} from "../constants/Constants";
 
 const AppRouter = () => {
-  const [isLogged, setIsLogged] = useState(false);
-  const userData = useSelector(state => state.auth.userData);
-  const token = useSelector(state => state.auth.token);
+  const mapState = (state) => ({
+    userData: state.auth.userData,
+    token: state.auth.token,
+    isLogged: state.auth.isLogged,
+  });
 
-  useEffect(() => {
-    if(userData && token){
-      setIsLogged(true);
-    }else{
-      setIsLogged(false);
+  const {
+    userData,
+    token,
+    isLogged,
+  } = useSelector(mapState);
+
+  if (isLogged) {
+    if (userData.role === ROLES.STUDENT) {
+      return (
+          <Switch>
+            <Route path='/student'>
+              <StudentCourses/>
+            </Route>
+            <Redirect from='/' to='/student'/>
+          </Switch>
+      )
+    } else if (userData.role === ROLES.ADMIN) {
+      return (
+          <Switch>
+            <Route path='/admin'>
+              <StudentCourses/>
+            </Route>
+          </Switch>
+      )
+    } else if (userData.role === ROLES.INSTRUCTOR) {
+      return (
+          <Switch>
+            <Route path='/instructor'>
+              <StudentCourses/>
+            </Route>
+          </Switch>
+      )
     }
-  }, [userData, token]);
-
-  if(isLogged){
+  } else {
     return (
-      <Switch>
-        <Route path='/'>
-          <StudentCourses />
-        </Route>
-      </Switch>
-    )
-  }else{
-    return (
-      <Switch>
-        <Route path='/register'>
-          <RegisterPage/>
-        </Route>
-        <Route path='/'>
-          <LoginPage/>
-        </Route>
-      </Switch>
+        <Switch>
+          <Route path='/register'>
+            <RegisterPage/>
+          </Route>
+          <Route path='/'>
+            <LoginPage/>
+          </Route>
+        </Switch>
     )
   }
 }
