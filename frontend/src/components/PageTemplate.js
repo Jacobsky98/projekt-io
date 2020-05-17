5a22614bacdc27d1b35ee05cd742f108f10b0be5
JedrzejDev <jedrzej.dev@gmail.com>
2020-05-17 17:15:49 +0200

    create Grades page

--- frontend/src/components/PageTemplate.js
+++ frontend/src/components/PageTemplate.js
@@ -1,44 +1,41 @@
-import React from 'react';
+import React from "react";
 import Grid from "@material-ui/core/Grid";
-import {useSelector} from "react-redux";
-import './PageTemplate.scss'
+import { useSelector } from "react-redux";
+import "./PageTemplate.scss";
 
-const PageTemplate = ({NavbarComponent = undefined, ...props}) => {
-    const loginAsStyles = {
-        textAlign: 'right',
-    };
-    const mapState = (state) => ({
-        userData: state.auth.userData,
-    });
+const PageTemplate = ({ NavbarComponent = undefined, ...props }) => {
+  const loginAsStyles = {
+    textAlign: "right",
+  };
+  const mapState = (state) => ({
+    userData: state.auth.userData,
+  });
 
-    let {
-        userData,
-    } = useSelector(mapState);
+  let { userData } = useSelector(mapState);
 
-    return (
-        <div className='page'>
-            <Grid container direction='column'>
-                <Grid container alignItems='flex-end'>
-                    <Grid item xs={12} style={loginAsStyles}>
-                        ZALOGOWANO JAKO: {`${userData.name} ${userData.surname} (${userData.role})`}
-                    </Grid>
-                </Grid>
-                <Grid container direction='row'>
-                    <Grid item xs={6}>
-                        <h1>DZIENNIK ELEKTRONICZNY</h1>
-                    </Grid>
-                    <Grid item xs={6} >
-                        {NavbarComponent && <NavbarComponent />}
-                    </Grid>
-                </Grid>
-                <div>
-                    {props.children}
-                </div>
+  return (
+    <div className="page">
+      <Grid container direction="column">
+        <Grid container alignItems="flex-end">
+          {userData && (
+            <Grid item xs={12} style={loginAsStyles}>
+              ZALOGOWANO JAKO:{" "}
+              {`${userData.name} ${userData.surname} (${userData.role})`}
             </Grid>
-        </div>
-    );
+          )}
+        </Grid>
+        <Grid container direction="row">
+          <Grid item xs={6}>
+            <h1>DZIENNIK ELEKTRONICZNY</h1>
+          </Grid>
+          <Grid item xs={6}>
+            {NavbarComponent && <NavbarComponent />}
+          </Grid>
+        </Grid>
+        <div>{props.children}</div>
+      </Grid>
+    </div>
+  );
 };
 
-export {
-    PageTemplate
-}
+export { PageTemplate };
