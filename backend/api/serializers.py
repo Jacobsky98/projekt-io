0c2dc8d248a96723b5140a77cd6629bcceab730a
Jakub Salamon <33131677+Jacobsky98@users.noreply.github.com>
2020-05-27 01:07:53 +0200

    Add endpoints

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,23 +1,110 @@
 from rest_framework import serializers
-from .models import Message, Course, Opinions, Annoucement
+from .models import Message, Course, Opinions, Annoucement, File, Grade, Task, Presence
+from django.utils import timezone
 
 class MessageSerializer(serializers.ModelSerializer):
+    title = serializers.CharField(max_length=100)
+    content = serializers.CharField(style={'base_template': 'textarea.html'})
+    date_send = serializers.DateTimeField(default=timezone.now)
+
     class Meta:
         model = Message
-        fields = ['id', 'sender', 'receiver', 'title', 'content', 'date_send']
+        fields = ['id', 'id_sender', 'id_receiver', 'title', 'content', 'date_send']
+
+    def create(self, validated_data):
+        instance = self.Meta.model(**validated_data)
+        instance.save()
+        return instance
 
 class AnnoucementSerializer(serializers.ModelSerializer):
+    date = serializers.DateTimeField(default=timezone.now)
+    title = serializers.CharField(max_length=50)
+    content = serializers.CharField(max_length=200)
+
     class Meta:
         model = Annoucement
         fields = ['id_course', 'date', 'title', 'content']
 
+    def create(self, validated_data):
+        instance = self.Meta.model(**validated_data)
+        instance.save()
+        return instance
+
 class CourseSerializer(serializers.ModelSerializer):
     annoucements = AnnoucementSerializer(many=True, read_only=True)
+    info = serializers.CharField(max_length=50)
+
     class Meta:
         model = Course
         fields = ['info', 'id_teacher', 'annoucements']
 
+    def create(self, validated_data):
+        instance = self.Meta.model(**validated_data)
+        instance.save()
+        return instance
+
+
+
 class OpinionsSerializer(serializers.ModelSerializer):
+    title = serializers.CharField(max_length=100)
+    content = serializers.CharField(style={'base_template': 'textarea.html'})
+
     class Meta:
         model = Opinions
         fields = ['id_sender', 'id_receiver', 'title', 'content']
+
+    def create(self, validated_data):
+        instance = self.Meta.model(**validated_data)
+        instance.save()
+        return instance
+
+
+class FileSerializer(serializers.ModelSerializer):
+    title = serializers.CharField(max_length=50)
+    file = serializers.FileField(default='files/tmp')
+    date_sent = serializers.DateTimeField()
+
+    class Meta:
+        model = File
+        fields = ['title', 'file', 'date_sent']
+
+    def create(self, validated_data):
+        instance = self.Meta.model(**validated_data)
+        instance.save()
+        return instance
+
+class GradeSerializer(serializers.ModelSerializer):
+    grade = serializers.IntegerField()
+
+    class Meta:
+        model = Grade
+        fields = ['id_student', 'id_task', 'grade']
+
+    def create(self, validated_data):
+        instance = self.Meta.model(**validated_data)
+        instance.save()
+        return instance
+
+class TaskSerializer(serializers.ModelSerializer):
+    deadline = serializers.DateTimeField(default=timezone.now)
+
+    class Meta:
+        model = Task
+        fields = ['id_course', 'deadline']
+
+    def create(self, validated_data):
+        instance = self.Meta.model(**validated_data)
+        instance.save()
+        return instance
+
+class PresenceSerializer(serializers.ModelSerializer):
+    was_present = serializers.BooleanField()
+
+    class Meta:
+        model = Presence
+        fields = ['id_student', 'id_course', 'was_present']
+
+    def create(self, validated_data):
+        instance = self.Meta.model(**validated_data)
+        instance.save()
+        return instance
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,19 +1,41 @@
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
-from .models import Message, Course, Opinions, Annoucement
-from .serializers import MessageSerializer, CourseSerializer, OpinionsSerializer, AnnoucementSerializer
+from rest_framework import permissions
+from .models import Message, Course, Opinions, File, Annoucement, Grade, Task, Presence
+from .serializers import MessageSerializer, CourseSerializer, OpinionsSerializer, FileSerializer, AnnoucementSerializer, GradeSerializer, TaskSerializer, PresenceSerializer
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 # Create your views here.
 
 
+class AnnoucementAPIView(APIView):
+    def get(self, request):
+        articles = Annoucement.objects.all()
+        serializer = AnnoucementSerializer(articles, many=True)
+        return Response(serializer.data)
+
+class AnnoucementCreate(APIView):
+    #TODO
+    permission_classes = (permissions.AllowAny,) # to trzeba zmienic
+
+    def post(self, request):
+        serializer = AnnoucementSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 class MessageAPIView(APIView):
     def get(self, request):
         articles = Message.objects.all()
         serializer = MessageSerializer(articles, many=True)
         return Response(serializer.data)
+
+class MessageCreate(APIView):
+    #TODO
+    permission_classes = (permissions.AllowAny,) # to trzeba zmienic
 
     def post(self, request):
         serializer = MessageSerializer(data=request.data)
@@ -23,17 +45,24 @@ class MessageAPIView(APIView):
         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class CourseAPIView(APIView):
+
     def get(self, request):
         articles = Course.objects.all()
         serializer = CourseSerializer(articles, many=True)
         return Response(serializer.data)
 
-    def post(self, request):
+class CourseCreate(APIView):
+    #TODO
+    permission_classes = (permissions.AllowAny,) # to trzeba zmienic
+
+    def post(self, request, format='json'):
         serializer = CourseSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            course = serializer.save()
+            if course:
+                json = serializer.data
+                return Response(json, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class CourseDetails(APIView):
     def get_object(self, id):
@@ -67,11 +96,6 @@ class OpinionsDetails(APIView):
         except Opinions.DoesNotExist:
             return HttpResponse(status=status.HTTP_404_NOT_FOUND)
 
-    def get(self, request, id):
-        article = self.get_object(id)
-        serializer = OpinionsSerializer(article)
-        return Response(serializer.data)
-
     def put(self, request, id):
         article = self.get_object(id)
         serializer = OpinionsSerializer(article, data=request.data)
@@ -80,7 +104,104 @@ class OpinionsDetails(APIView):
             return Response(serializer.data)
         return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def get(self, request, id):
+        article = self.get_object(id)
+        serializer = OpinionsSerializer(article)
+        return Response(serializer.data)
+
     def delete(self, request, id):
         article = self.get_object(id)
         article.delete()
         return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+
+class OpinionsCreate(APIView):
+    #TODO
+    permission_classes = (permissions.AllowAny,) # to trzeba zmienic
+
+    def post(self, request, format='json'):
+        serializer = OpinionsSerializer(data=request.data)
+        if serializer.is_valid():
+            opinions = serializer.save()
+            if opinions:
+                json = serializer.data
+                return Response(json, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class FileAPIView(APIView):
+    def get(self, request):
+        articles = File.objects.all()
+        serializer = FileSerializer(articles, many=True)
+        return Response(serializer.data)
+
+class FileCreate(APIView):
+    #TODO
+    permission_classes = (permissions.AllowAny,) # to trzeba zmienic
+
+    def post(self, request, format=None):
+        serializer = FileSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class GradeAPIView(APIView):
+
+    def get(self, request):
+        articles = Grade.objects.all()
+        serializer = GradeSerializer(articles, many=True)
+        return Response(serializer.data)
+
+class GradeCreate(APIView):
+    #TODO
+    permission_classes = (permissions.AllowAny,) # to trzeba zmienic
+
+    def post(self, request, format='json'):
+        serializer = GradeSerializer(data=request.data)
+        if serializer.is_valid():
+            grade = serializer.save()
+            if grade:
+                json = serializer.data
+                return Response(json, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class TaskAPIView(APIView):
+
+    def get(self, request):
+        articles = Task.objects.all()
+        serializer = TaskSerializer(articles, many=True)
+        return Response(serializer.data)
+
+class TaskCreate(APIView):
+    #TODO
+    permission_classes = (permissions.AllowAny,) # to trzeba zmienic
+
+    def post(self, request, format='json'):
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            task = serializer.save()
+            if task:
+                json = serializer.data
+                return Response(json, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PresenceAPIView(APIView):
+
+    def get(self, request):
+        articles = Presence.objects.all()
+        serializer = PresenceSerializer(articles, many=True)
+        return Response(serializer.data)
+
+class PresenceCreate(APIView):
+    #TODO
+    permission_classes = (permissions.AllowAny,) # to trzeba zmienic
+
+    def post(self, request, format='json'):
+        serializer = PresenceSerializer(data=request.data)
+        if serializer.is_valid():
+            presence = serializer.save()
+            if presence:
+                json = serializer.data
+                return Response(json, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
